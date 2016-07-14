371b98e3208008c8c9b3ba37fa1f94ff2ce83164
gchriswill <gchriswill@me.com>
2016-07-14 16:44:23 -0400

    Day three commit: Code Cleaning installer.rb for issue #2
    In this commit, I’m updating and removing unnecessary comments, some
    code snippets and including the team members’s signatures in the
    installer.rb file located in the Tools directory…

--- Tools/installer.rb
+++ Tools/installer.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby -wU
 
-###################################################################
-# make an installer for Soundflower
-# requires: you must have already performed a Deployment build
-###################################################################
+# Code Implementations/Modifications by Christopher Gonzalez D.K.A "gchriswill" at Parthenon Studio
+# email: gchriswill -at- me -dot- com
+# website: N/A
+#
+# Project Management by Elianna Bentz "Eli" at Parthenon Studio
+# email: eliannabent -at- gmail -dot- com
+# website: N/A
+
+####################################################################################
+# Requirment: Must have already performed a Deployment build in order to be executed
+# inside of the root directory of the project repository.
+# Terminal Command Example: ./build.rb -dep
+####################################################################################
 
 require 'open3'
 require 'fileutils'
@@ -11,14 +20,12 @@ require 'pathname'
 require 'rexml/document'
 include REXML
 
-# This finds our current directory, to generate an absolute path for the require
 libdir = "."
-Dir.chdir libdir        # change to libdir so that requires work
+Dir.chdir libdir
 
 @svn_root = ".."
 Dir.chdir @svn_root
 @svn_root = Dir.pwd
-
 
 ###################################################################
 # sub routines
@@ -26,14 +33,14 @@ Dir.chdir @svn_root
 
 def create_logs
   @build_log = File.new("#{@svn_root}/Installer/_installer.log", "w")
-  @build_log.write("SOUNDFLOWER INSTALLER LOG: #{`date`}\n\n")
+  @build_log.write("Artisyns51 INSTALLER LOG: #{`date`}\n\n")
   @build_log.flush
   @error_log = File.new("#{@svn_root}/Installer/_error.log", "w")
-  @error_log.write("SOUNDFLOWER INSTALLER ERROR LOG: #{`date`}\n\n")
+  @error_log.write("Artisyns51 INSTALLER ERROR LOG: #{`date`}\n\n")
   @error_log.flush
   trap("SIGINT") { die }
 end
-  
+
 def die
   close_logs
   exit 0
@@ -56,12 +63,10 @@ def log_error(str)
   @error_log.flush
 end
 
-
-# This defines a wrapper that we use to call shell commands
 def cmd(commandString)
   out = ""
   err = ""
-  
+
   Open3.popen3(commandString) do |stdin, stdout, stderr|
     out = stdout.read
     err = stderr.read
@@ -74,45 +79,44 @@ end
 def getversion()
   theVersion = "0.0.0"
 
-  f = File.open("#{@installer_root}/System/Library/Extensions/Soundflower.kext/Contents/Info.plist", "r")
+  f = File.open("#{@installer_root}/System/Library/Extensions/Artisyns51.kext/Contents/Info.plist", "r")
   str = f.read
   theVersion = str.match(/<key>CFBundleShortVersionString<\/key>\n.*<string>(.*)<\/string>/).captures[0]
   f.close
-  
+
   puts"  version: #{theVersion}"
   return theVersion;
 end
 
 ###################################################################
-# here is where we actually build the installer
+# Installer Builder Snippets
 ###################################################################
 
 create_logs()
 
 @installer_root = "#{@svn_root}/Build/InstallerRoot"
 @version = getversion()
-@build_folder = "#{@svn_root}/Build/Soundflower-#{@version}"
+@build_folder = "#{@svn_root}/Build/Artisyns51-#{@version}"
 
 puts "  Creating installer directory structure..."
-cmd("rm -rfv \"#{@build_folder}\"")                       # remove an old temp dir if it exists
-cmd("mkdir -pv \"#{@build_folder}\"")                     # now make a clean one, and build dir structure in it
+cmd("rm -rfv \"#{@build_folder}\"")
+cmd("mkdir -pv \"#{@build_folder}\"")
 
-cmd("cp \"#{@svn_root}/Tools/Uninstall Soundflower.scpt\"           \"#{@installer_root}\"/Applications/Soundflower")
-cmd("cp \"#{@svn_root}/License.txt\"                                \"#{@installer_root}\"/Applications/Soundflower")
-cmd("cp \"#{@svn_root}/Installer/ReadMe.rtf\"                       \"#{@installer_root}\"/Applications/Soundflower")
-cmd("cp \"#{@svn_root}/SoundflowerBed/Soundflowerbed README.rtf\"   \"#{@installer_root}\"/Applications/Soundflower")
+cmd("cp \"#{@svn_root}/Tools/Uninstall Artisyns51.scpt\"           \"#{@installer_root}\"/Applications/Artisyns51")
+cmd("cp \"#{@svn_root}/License.txt\"                                \"#{@installer_root}\"/Applications/Artisyns51")
+cmd("cp \"#{@svn_root}/Installer/ReadMe.rtf\"                       \"#{@installer_root}\"/Applications/Artisyns51")
 
 puts "  Building Package -- this could take a while..."
-puts `pkgbuild --root \"#{@installer_root}\" --identifier com.cycling74.soundflower --version #{@version} --install-location "/" \"#{@build_folder}/Soundflower.pkg\" --ownership preserve  --scripts \"#{@svn_root}/Installer/scripts\" --sign \"Developer ID Installer: Cycling '74\"`
+puts `pkgbuild --root \"#{@installer_root}\" --identifier me.gchriswill.artisyns51 --version #{@version} --install-location "/" \"#{@build_folder}/Artisyns51.pkg\" --ownership preserve  --scripts \"#{@svn_root}/Installer/scripts\" --sign \"Developer ID Installer: me.gchriswill.artisyns51\"`
 
 puts "  Copying readme, license, etc...."
 cmd("cp \"#{@svn_root}/License.txt\" \"#{@build_folder}\"")
 cmd("cp \"#{@svn_root}/Installer/ReadMe.rtf\" \"#{@build_folder}\"")
-cmd("cp \"#{@svn_root}/Tools/Uninstall Soundflower.scpt\" \"#{@build_folder}\"")
+cmd("cp \"#{@svn_root}/Tools/Uninstall Artisyns51.scpt\" \"#{@build_folder}\"")
 
 puts "  Creating Disk Image..."
-cmd("rm -rfv \"#{@svn_root}/Installer/Soundflower-#{@version}.dmg\"")
-cmd("hdiutil create -srcfolder \"#{@build_folder}\" \"#{@svn_root}/Build/Soundflower-#{@version}.dmg\"")
+cmd("rm -rfv \"#{@svn_root}/Installer/Artisyns51-#{@version}.dmg\"")
+cmd("hdiutil create -srcfolder \"#{@build_folder}\" \"#{@svn_root}/Build/Artisyns51-#{@version}.dmg\"")
 
 puts "  All done!"
 
